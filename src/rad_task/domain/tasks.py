3c7bdb541ab88c0430c0c97542dc80be1aa877d5
Thiago Hiromi Santana <thiago.hiromi@gmail.com>
2022-05-30 23:50:19 +0200

    Mark description field as nullable

--- src/rad_task/domain/tasks.py
+++ src/rad_task/domain/tasks.py
@@ -10,7 +10,7 @@ class Task(Base):
 
     id = sa.Column(sa.Integer, primary_key=True)
     title = sa.Column(sa.String(255), nullable=False)
-    description = sa.Column(sa.Text, nullable=False)
+    description = sa.Column(sa.Text, nullable=True)
     done = sa.Column(sa.Boolean, default=False)
     created_at = sa.Column(sa.DateTime, default=sa.func.now())
     updated_at = sa.Column(sa.DateTime, default=sa.func.now(), onupdate=sa.func.now())
